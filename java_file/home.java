a494667018734fd83a96164daefdc1bb6924cb37
ezaaysf <ezayusuf2002@gmail.com>
2025-01-04 10:14:27 +0700

    Add files via upload

---
+++ java_file/home.java
@@ -0,0 +1,55 @@
+package com.example.growsfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ImageView btnhome = findViewById(R.id.fhome);
+        ImageView btnfav = findViewById(R.id.ffavorite);
+        ImageView btncart = findViewById(R.id.cart);
+        ImageView btnprofile = findViewById(R.id.profile);
+
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnfav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, favorite.class);
+                startActivity(intent);
+            }
+        });
+
+        btncart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, cart.class);
+                startActivity(intent);
+            }
+        });
+
+        btnprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
